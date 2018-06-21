5c696840ea92a7950ee2e80b93b186a492dad452
Jason Lengstorf <jason@lengstorf.com>
2018-06-21 13:50:14 -0700

    feat: update store copy
    - let people know the store isn’t ready yet
    - spread a little love
    
    fix #2

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ProductListings from './ProductListings';
-import { Heading, Lede } from '../shared/Typography';
+import { Heading, Lede, Text } from '../shared/Typography';
 
 export default () => (
   <>
     <Heading>Get Gatsby Swag</Heading>
     <Lede>
-      TKTK write a fun description with a CTA for non-contributors to get
-      involved for free swag.
+      <strong>🚨 NOTE 🚨</strong> This store is not quite ready for prime time.
     </Lede>
+    <Text>
+      We’re still waiting on orders to arrive and haven’t worked all the bugs
+      out yet. Technically, you <em>can</em> buy things, but they won’t ship
+      until mid-July.
+    </Text>
+    <Text>
+      P.S. — We 💜 you for paying close enough attention to our GitHub org to
+      know that this store exists.
+    </Text>
     <ProductListings />
   </>
 );
