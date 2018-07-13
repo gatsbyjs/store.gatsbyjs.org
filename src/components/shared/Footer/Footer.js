657a86ad451b4ed606ec1eb686906eeefc0a240a
Florian Kissling <florian.kissling@gmail.com>
2018-07-13 23:13:04 +0200

    Use `link` styles for footer links

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'react-emotion';
 import About from './About';
 import metaball from './metaball.svg';
-import { colors, spacing } from '../../../utils/styles';
+import { colors, spacing, link } from '../../../utils/styles';
 
 const FooterContainer = styled('footer')`
   background: url(${metaball});
@@ -23,6 +23,10 @@ const LegalInfo = styled('p')`
   margin-bottom: ${spacing['2xl'] * 2}px;
 `;
 
+const Link = styled('a')`
+  ${link};
+`;
+
 export default ({ displayAbout }) => (
   <FooterContainer>
     <Footer>
@@ -32,10 +36,10 @@ export default ({ displayAbout }) => (
         <span role="img" aria-label="love">
           💜
         </span>{' '}
-        by the <a href="https://www.gatsbyjs.com">Gatsby Inkteam</a> ·{' '}
-        <a href="https://github.com/gatsbyjs/store.gatsbyjs.org">
+        by the <Link href="https://www.gatsbyjs.com">Gatsby Inkteam</Link> ·{' '}
+        <Link href="https://github.com/gatsbyjs/store.gatsbyjs.org">
           See the source code on GitHub
-        </a>
+        </Link>
       </LegalInfo>
     </Footer>
   </FooterContainer>
