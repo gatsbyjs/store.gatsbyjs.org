6841581a417fde6f2fe65349ab227f57d1e6530e
Florian Kissling <florian.kissling@gmail.com>
2018-07-13 12:46:18 +0200

    Add „About the Gatsby Store“

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Layout from '../components/shared/Layout';
 import { Heading, Text } from '../components/shared/Typography';
 
-const NotFoundPage = () => (
-  <Layout>
+const NotFoundPage = ({ location }) => (
+  <Layout location={location}>
     <Heading>Whoops — That Page Doesn’t Exist (404)</Heading>
     <Text>
       Looks like the page you requested either doesn’t exist or has been moved.
