76de925d9d9845f2fdb20cc8d01e1c5d95589e5b
Caleb Sharp <epicsharp11@gmail.com>
2018-10-13 17:03:11 -0700

    Make auth callback page nicer

--- src/pages/callback.js
+++ src/pages/callback.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { push } from 'gatsby';
+import styled from 'react-emotion';
 import { handleAuthentication } from '../utils/auth';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+  overflow: none;
+`;
+
+const Message = styled.h1`
+  font-family: 'Futura PT', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+`;
 
 export default () => {
   handleAuthentication(() => push('/account/dashboard'));
 
-  return <p>Logging you in...</p>;
+  return (
+    <Container>
+      <Message>Logging you in...</Message>
+    </Container>
+  );
 };
