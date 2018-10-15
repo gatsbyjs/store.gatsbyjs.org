4fc0d85f3d27f8e71208ee1d9c0e67e6fb51a361
Caleb Sharp <epicsharp11@gmail.com>
2018-10-15 09:28:00 -0700

    feat: make auth callback page nicer (#151)
    Currently, the auth callback page just displays some text in the default system font, which I thought was kind of ugly, so I decided to make it look a bit nicer 😄!
    
    Screenshot:
    ![screen shot 2018-10-13 at 5 05 09 pm](https://user-images.githubusercontent.com/8854811/46911231-30ca7f80-cf0a-11e8-889b-c2c96d8d72c8.png)

--- src/pages/callback.js
+++ src/pages/callback.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import { push } from 'gatsby';
+import styled from 'react-emotion';
 import { handleAuthentication } from '../utils/auth';
+import { colors, fonts } from '../utils/styles';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+  overflow: none;
+`;
+
+const Message = styled.h1`
+  color: ${colors.darkest};
+  font-family: ${fonts.heading};
+`;
 
 export default () => {
   handleAuthentication(() => push('/account/dashboard'));
 
-  return <p>Logging you in...</p>;
+  return (
+    <Container>
+      <Message>Logging you in...</Message>
+    </Container>
+  );
 };
