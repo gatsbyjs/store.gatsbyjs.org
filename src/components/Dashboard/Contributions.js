3565d29b51cb41433d160c92b051573a164ea5c3
Jason Lengstorf <jason@lengstorf.com>
2018-10-30 13:50:41 -0700

    feat: convert to Apollo on the client side

--- src/components/Dashboard/Contributions.js
+++ src/components/Dashboard/Contributions.js
@@ -1,23 +1,8 @@
 import React from 'react';
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
 import UserContext from '../../context/UserContext';
 import DiscountCode from '../DiscountCode/DiscountCode';
 import IssueList from './IssueList';
 import { Heading, Subheading, Lede, Text } from '../shared/Typography';
-
-const GET_CONTRIBUTIONS = gql`
-  {
-    contributorInformation(githubUsername: "jlengstorf") {
-      totalContributions
-      pullRequests {
-        title
-        url
-        number
-      }
-    }
-  }
-`;
 
 export default () => (
   <UserContext.Consumer>
@@ -29,24 +14,6 @@ export default () => (
             Your Most Recent Contribution
           </Subheading>
           <IssueList issues={contributions.issues.slice(0, 1)} />
-          <Query query={GET_CONTRIBUTIONS}>
-            {({ loading, error, data }) => {
-              if (loading) return 'Loading...';
-              if (error) return `Error! ${error.message}`;
-
-              return (
-                <ul>
-                  {data.contributorInformation.pullRequests.map(pr => (
-                    <li key={pr.url}>
-                      <a href={pr.url}>
-                        #{pr.number}: {pr.title}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              );
-            }}
-          </Query>
         </>
       ) : (
         <>
