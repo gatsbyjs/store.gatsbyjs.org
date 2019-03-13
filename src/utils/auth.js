8408befdc9be0e7d75eff798be9a076f499a27c0
Matt Barraza-Carl <mbarrazacarl@launchdarkly.com>
2019-03-12 21:42:51 -0700

    lint: Arrow function expected no return value

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 import auth0js from 'auth0-js';
 
-export const isBrowser = typeof window !== 'undefined';
+export const isBrowser = typeof window !== `undefined`;
 
 // To speed things up, we’ll keep the profile stored unless the user logs out.
 // This prevents a flicker while the HTTP round-trip completes.
@@ -13,8 +13,8 @@ const auth0 = isBrowser
       clientID: process.env.AUTH0_CLIENTID,
       redirectUri: process.env.AUTH0_CALLBACK,
       audience: process.env.AUTH0_AUDIENCE,
-      responseType: 'token id_token',
-      scope: 'openid profile email'
+      responseType: `token id_token`,
+      scope: `openid profile email`,
     })
   : {};
 
@@ -28,9 +28,9 @@ export const login = () => {
 
 export const logout = callback => {
   if (isBrowser) {
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('expires_at');
+    localStorage.removeItem(`access_token`);
+    localStorage.removeItem(`id_token`);
+    localStorage.removeItem(`expires_at`);
   }
 
   // Remove the locally cached profile to avoid confusing errors.
@@ -48,11 +48,11 @@ const setSession = authResult => {
     authResult.expiresIn * 1000 + new Date().getTime()
   );
 
-  localStorage.setItem('access_token', authResult.accessToken);
-  localStorage.setItem('id_token', authResult.idToken);
-  localStorage.setItem('expires_at', expiresAt);
+  localStorage.setItem(`access_token`, authResult.accessToken);
+  localStorage.setItem(`id_token`, authResult.idToken);
+  localStorage.setItem(`expires_at`, expiresAt);
 
-  return true;
+  return;
 };
 
 export const handleAuthentication = callback => {
@@ -76,20 +76,20 @@ export const isAuthenticated = () => {
     return false;
   }
 
-  let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+  let expiresAt = JSON.parse(localStorage.getItem(`expires_at`));
   return new Date().getTime() < expiresAt;
 };
 
 export const getAccessToken = () => {
   if (!isBrowser) {
-    return '';
+    return ``;
   }
 
-  return localStorage.getItem('access_token');
+  return localStorage.getItem(`access_token`);
 };
 
-export const getUserInfo = () => {
-  return new Promise((resolve, reject) => {
+export const getUserInfo = () =>
+  new Promise((resolve, reject) => {
     // If the user has already logged in, don’t bother fetching again.
     if (profile) {
       resolve(profile);
@@ -113,4 +113,3 @@ export const getUserInfo = () => {
       resolve(profile);
     });
   });
-};
