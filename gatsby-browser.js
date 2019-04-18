17c5d57fb96a30ea266aa0ff482ed110bbad55e1
Sam Julien <spjulien@gmail.com>
2019-04-18 12:46:09 -0700

    refactor: move auth from local storage to memory

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { client } from './src/context/ApolloContext';
+import { silentAuth } from './src/utils/auth';
+
+class SessionCheck extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
+  handleCheckSession = () => {
+    this.setState({ loading: false });
+  };
+
+  componentDidMount() {
+    silentAuth(this.handleCheckSession);
+  }
+
+  render() {
+    return (
+      this.state.loading === false && (
+        <React.Fragment>{this.props.children}</React.Fragment>
+      )
+    );
+  }
+}
 
 export const wrapRootElement = ({ element }) => (
-  <ApolloProvider client={client}>{element}</ApolloProvider>
+  <SessionCheck>
+    <ApolloProvider client={client}>{element}</ApolloProvider>
+  </SessionCheck>
 );
