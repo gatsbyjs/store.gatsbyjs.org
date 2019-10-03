b23155d0a04a38e90219acc27275d32df23248f6
Saksham Saxena <sakshamappdev@gmail.com>
2019-10-03 18:38:46 +0530

    Made the 404 page more informative.
    I addressed this in issue #290

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,14 +3,21 @@ import { Heading, Text, TextContainer } from '../components/shared/Typography';
 
 const NotFoundPage = () => (
   <TextContainer>
-    <Heading>Whoops - That Page Doesn’t Exist (404)</Heading>
+    <Heading>Whoops! - This Page Doesn’t Exist (404)</Heading>
     <Text>
       Looks like the page you requested either doesn’t exist or has been moved.
       If you think this is an error or ended up at this page by following a
       link, please{' '}
       <a href="https://github.com/gatsbyjs/gatsby/issues/new">open an issue</a>{' '}
       to let us know.
+      {"\n"}
+      <a href="https://store.gatsbyjs.org">Home - Swag store</a>{"\n"}
+ {"\n"}
+      <a href="https://www.gatsbyjs.org">Home - Gatsby JS</a>{"\n"}
+ {"\n"}
+    Contribute to Gatsby JS on {' '} <a href="https://www.gatsbyjs.org">Github</a>{"\n"}
     </Text>
+
   </TextContainer>
 );
 
