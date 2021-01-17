de286210ab85db08119bacf2666184880b61c818
Florian Kissling <florian.kissling@gmail.com>
2021-01-17 22:14:17 +0100

    Show logged in user avatar

--- src/components/ContributorArea/LogoutBar.js
+++ src/components/ContributorArea/LogoutBar.js
@@ -4,7 +4,13 @@ import styled from '@emotion/styled';
 
 import { Button } from '../shared/Buttons';
 
-import { colors, spacing, animations, fontSizes } from '../../utils/styles';
+import {
+  colors,
+  spacing,
+  animations,
+  fontSizes,
+  radius
+} from '../../utils/styles';
 
 const LogoutBarRoot = styled(`div`)`
   align-items: flex-start;
@@ -14,6 +20,10 @@ const LogoutBarRoot = styled(`div`)`
   margin-bottom: ${spacing.lg}px;
 `;
 
+const User = styled(`div`)`
+  align-items: flex-start;
+  display: flex;
+`;
 const Info = styled(`div`)`
   color: ${colors.brandBright};
   font-size: ${fontSizes.md};
@@ -28,14 +38,44 @@ const Info = styled(`div`)`
 const Logout = styled(Button)`
   flex-grow: 0;
   padding: ${spacing.xs}px ${spacing.sm}px;
+  background: ${colors.lightest};
+  color: ${colors.text};
+`;
+
+const AvatarContainer = styled('span')`
+  display: block;
+  position: relative;
+  margin-right: ${spacing.md}px;
+
+  :before {
+    border-radius: ${radius.md}px;
+    content: ' ';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+  }
 `;
 
 const LogoutBar = ({ error, loading, profile, handleLogout }) =>
   !loading && !error ? (
     <LogoutBarRoot>
-      <Info>
-        Logged in as <b>@{profile.nickname}</b>
-      </Info>
+      <User>
+        <AvatarContainer>
+          <img
+            css={{ display: 'block' }}
+            src={profile.picture}
+            height={spacing['3xl']}
+            width={spacing['3xl']}
+          />
+        </AvatarContainer>
+        <Info>
+          Logged in as <b>@{profile.nickname}</b>
+        </Info>
+      </User>
       <Logout onClick={handleLogout} inverse>
         Log out
       </Logout>
