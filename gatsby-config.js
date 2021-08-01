5f546bb5fd5526919e84f6672e8cc42f80e063b8
Florian Kissling <florian.kissling@gmail.com>
2021-08-01 17:28:21 +0200

    Remove seemingly obsolete `apiKey` option …
    `gatsby-source-shopify` says it doesn't know it.

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,6 @@ module.exports = {
     {
       resolve: 'gatsby-source-shopify',
       options: {
-        apiKey: process.env.SHOPIFY_ADMIN_API_KEY,
         password: process.env.SHOPIFY_ADMIN_PASSWORD,
         storeUrl: process.env.SHOPIFY_SHOP_ADDRESS
       }
